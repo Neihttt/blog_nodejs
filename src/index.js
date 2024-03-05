24ab500277d647ebede3619f5b97ddf8b6c7e93b
THIEN\THANH THIEN <thanhthien160501@gmail.com>
2024-03-05 16:49:36 +0700

    set up MVC

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template Engine
 app.engine('hbs', engine({
@@ -18,13 +24,11 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
+
+
