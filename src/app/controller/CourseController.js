40dc18289460921955f07d6310824ab24ad6d78b
THIEN\THANH THIEN <thanhthien160501@gmail.com>
2024-05-18 08:29:21 +0700

    soft delete

--- src/app/controller/CourseController.js
+++ src/app/controller/CourseController.js
@@ -22,12 +22,11 @@ class CourseController {
 
     // [POST]courses/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg`;
+        req.body.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg`;
 
-        const course = new Course(formData);
+        const course = new Course(req.body);
         course.save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(error => {
 
                 next(error);
@@ -56,10 +55,33 @@ class CourseController {
 
     // [DELETE]courses/:id
     delete(req, res, next) {
-
-        Course.deleteOne({ _id: req.params.id })
+ 
+        Course.delete({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
+
+    }
+    // [DELETE]courses/:id/force
+    forceDelete(req, res, next){
+        Course.deleteOne({ _id: req.params.id })
+        .then(() => res.redirect('back'))
+        .catch(next);
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next){
+
+        Course.restore({ _id: req.params.id })
+        .then(() => res.redirect('back'))
+        .catch(next);
+        // Course.findWithDeleted({ deleted: true}).lean()
+        // .then((courses) =>{
+        //     res.render('me/trash-courses', {
+        //         title: "delete",
+        //         courses: courses,
+        //     })
+        // })
+        // .catch(next);
 
     }
 
--- src/app/controller/MeController.js
+++ src/app/controller/MeController.js
@@ -5,13 +5,25 @@ const { mongooseToObject, mutipleMongooseToObject } = require('../../util/mongoo
 class MeController {
     //GET/me/stored/courses
 
-    storedCourses(req, res,next) {
-        Course.find({})
-        .then(courses => res.render('me/stored-courses', {
-            courses: mutipleMongooseToObject(courses)
-        }))
-        .catch(next);
-       
+    storedCourses(req, res, next) {
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => res.render('me/stored-courses', {
+
+                deletedCount,
+                courses: mutipleMongooseToObject(courses)
+                })
+            )
+            .catch(next)
+
+    }
+    //GET/me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then(courses => res.render('me/trash-courses', {
+                courses: mutipleMongooseToObject(courses)
+            }))
+            .catch(next);
     }
 
 }
